39df516d99cf5f139e0ce5c89bf1979397b2baa2
TreyComic <trey.greene1153@gmail.com>
2024-04-07 19:28:16 -0400

    method whatChaMaCallIt complete

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -15,8 +15,22 @@ public class StringsAndThings {
      *           countYZ("day fyyyz"); // Should return 2
      */
     public Integer countYZ(String input){
-        return null;
+        input = input.toLowerCase(); //makes all input as string easier to handle
+        int wordCounter = 0;
+        for(String wordToCompare : input.split(" ")){ //checks until a space is found then splits them into sub strings
+            char lastCharacter = wordToCompare.charAt(wordToCompare.length() - 1);
+            //charAt refers the character in which the index (parameters) is in reference to
+            //wordToCompare.length gives length of the words but subtract 1
+            //this counter removes 1 because the index starts at 0
+
+            if(lastCharacter == 'y' || lastCharacter == 'z'){
+                wordCounter ++; //add if the word ends with y or z
+            }
+        }
+        return wordCounter;
     }
+
+
 
     /**
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
@@ -28,8 +42,14 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        if(remove == null){
+            return base;
+        }
+        return base.replace(remove, "");
+        // replaces anythind from remove in the base and replaces it with nothing!
     }
+
+
 
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
@@ -40,8 +60,15 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
     public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+        int counterIs = input.split("is", -1).length -1;
+        //inputsplit stops at is and creates its substrings
+        //-1 for empty strings
+        //length - 1 is because index starts at 0
+        int counterNot = input.split("not", -1).length -1;
+        return counterIs == counterNot;
     }
+
+
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
@@ -51,8 +78,18 @@ public class StringsAndThings {
      *           gHappy("xxggyygxx") // Should return  false
      */
     public Boolean gIsHappy(String input){
-        return null;
+        for(int iterate = 0; iterate < input.length(); iterate++){
+            if(input.charAt(iterate) == 'g') {
+                //check if there is not or is another 'g'
+                if((iterate == 0 || input.charAt(iterate- 1) != 'g') && (iterate == input.length()-1 || input.charAt(iterate +1) != 'g')){
+                    //checking g for left and then for right
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
 
 
     /**
@@ -63,6 +100,14 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
+        int counter = 0;
+        for(int i = 0; i < input.length() - 2; i++){ //third to last char in the string!
+            char currentCharacter = input.charAt(i);
+            if(currentCharacter == input.charAt(i + 1) && currentCharacter == input.charAt(i + 2)){
+                counter++;
+            }
+            return counter;
+        }
         return null;
     }
 }
